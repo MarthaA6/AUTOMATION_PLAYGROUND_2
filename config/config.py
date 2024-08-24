6052cce08d34201e30c32daa07213ee9b67140e1
Martha Asoanya <129972923+MarthaA6@users.noreply.github.com>
2024-08-24 18:08:55 +0100

    update AUTOMATION_PLAYGROUND_2

---
+++ config/config.py
@@ -0,0 +1,14 @@
+class Config:
+    BaseUrl = "https://automationplayground.com/crm/login.html"
+    USERNAME = "martha@gmail.com"
+    PASSWORD = "pass_code"
+
+
+class ConfigNewCustomerForm:
+    EMAIL_ADDRESS = "test@123.com"
+    FIRST_NAME = "house"
+    LAST_NAME = "food"
+    CITY = "lagos"
+    STATE = "lagos"
+
+
--- test_suite.py
+++ test_suite.py
@@ -5,6 +5,7 @@ from Login_Page.LoginPage import LoginPage
 from Customer_Form_Page.NewCustomerPage import NewCustomerPageForm
 from New_Customer_Button_Page.NewCustomerButtonPage import NewCustomerButtonPage
 from Sign_Out_Page.SignOutPage import SignOutPage
+from config.config import Config, ConfigNewCustomerForm
 
 
 @pytest.fixture(scope="session")
@@ -20,13 +21,13 @@ def driver_setup():
 def login(driver_setup):
     driver = driver_setup
     login_page = LoginPage(driver)
-    login_page.login_url("https://automationplayground.com/crm/login.html")
+    login_page.login_url(Config.BaseUrl)
     return login_page
 
 
 def test_login_page_automation_playground(login):
-    login.email("martha@gmail.com")
-    login.password("pass_code")
+    login.email(Config.USERNAME)
+    login.password(Config.PASSWORD)
     login.submit()
 
 
@@ -37,11 +38,11 @@ def test_new_customer_button_on_automation_playground(login):
 
 def test_new_customer_form_page_on_automation_playground(login):
     new_customer_form = NewCustomerPageForm(login.driver)
-    new_customer_form.enter_email_address("test@123.com")
-    new_customer_form.enter_first_name("house")
-    new_customer_form.enter_last_name("food")
-    new_customer_form.enter_city("lagos")
-    new_customer_form.enter_state("lagos")
+    new_customer_form.enter_email_address(ConfigNewCustomerForm.EMAIL_ADDRESS)
+    new_customer_form.enter_first_name(ConfigNewCustomerForm.FIRST_NAME)
+    new_customer_form.enter_last_name(ConfigNewCustomerForm.LAST_NAME)
+    new_customer_form.enter_city(ConfigNewCustomerForm.CITY)
+    new_customer_form.enter_state(ConfigNewCustomerForm.STATE)
     new_customer_form.click_gender_check_box()
     new_customer_form.click_submit_button()
 
